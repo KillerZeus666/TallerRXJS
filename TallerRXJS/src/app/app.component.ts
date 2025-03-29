8114c7d9bc9016b4cb42eb469ffd1736878b4137
KillerZeus666 <katheryng2017@gmail.com>
2025-03-29 09:03:02 -0500

    Agregando conexion a la API

--- TallerRXJS/src/app/app.component.ts
+++ TallerRXJS/src/app/app.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from './models/User';
 
 @Component({
   selector: 'app-root',
@@ -9,9 +12,22 @@ export class AppComponent {
   title = 'TallerRXJS';
   ROOT_URL = "https://dummyjson.com";
 
-  // Declaramos la variable correctamente
+  // Para Realizar Peticiones HTTP
+  constructor(private http: HttpClient) {}
+  
+  // 1. Crear un Observable
+  user$: Observable<any> = new Observable();
+
+  usuario: User | null = null;
+
+  // Declaramos la variable para almacenar el usuario
   txUser: string = "";
 
   searchUser() {
+    this.user$ = this.http.get<User>(`${this.ROOT_URL}/users/1`);
+    // Suscribirnos a dicha petición
+    this.user$.subscribe((userInfo) => {
+      this.usuario = userInfo;
+    });
   }
 }
